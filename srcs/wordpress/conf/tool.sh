2ccd362f574655c373293521163633024da69d6d
LoisDuplain <loisdupl42@gmail.com>
2022-02-24 16:27:40 +0100

    Use ENV vars to create wordpress

--- srcs/wordpress/conf/tool.sh
+++ srcs/wordpress/conf/tool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wp core download --allow-root
-wp core config --dbname='inception1' --dbuser='lduplain' --dbpass='pouet' --dbhost='frweb4.pulseheberg.net:3306' --dbprefix='wp_' --allow-root
-wp core install --url="lduplain.42.fr" --title="lduplain inception website" --admin_user="lduplain" --admin_password="superprout" --admin_email="lduplain@student.42lyon.fr" --allow-root
-wp user create toto toto@toto.com --role='subscriber' --user_pass='prout' --allow-root
+wp core config --dbname="$DB_DB" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST":"$DB_PORT" --dbprefix='wp_' --allow-root
+wp core install --url="$WP_ADMIN_USER.42.fr" --title="$WP_ADMIN_USER inception website" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_USER@student.42lyon.fr" --allow-root
+wp user create $WP_DEFAULT_USER "$WP_DEFAULT_USER"@randomuser.com --role='subscriber' --user_pass="$WP_DEFAULT_PASS" --allow-root
 wp option update comment_registration 1 --allow-root
 exec /usr/sbin/php-fpm7.3 -F
