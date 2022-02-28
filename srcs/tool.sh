a4b3b211bd32992f399ec6bec4687eb88acda988
LoisDuplain <loisdupl42@gmail.com>
2022-02-28 17:01:03 +0100

    This is the end 2

--- srcs/tool.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wp core download --allow-root
-wp core config --dbname='inception1' --dbuser='lduplain' --dbpass='pouet' --dbhost='frweb4.pulseheberg.net:3306' --dbprefix='wp_' --allow-root
-wp core install --url="lduplain.42.fr" --title="lduplain inception website" --admin_user="lduplain" --admin_password="superprout" --admin_email="lduplain@student.42lyon.fr" --allow-root
-wp user create toto toto@toto.com --role='subscriber' --user_pass='prout' --allow-root
-wp option update comment_registration 1 --allow-root
-exec /usr/sbin/php-fpm7.3 -F
